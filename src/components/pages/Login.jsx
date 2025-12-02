871a42e5ba712b0ea7a87eb55417715ee71226c1
Marcos Lautaro Molina <marcoslautaromolina@gmail.com>
2025-12-02 05:58:44 -0300

    hice la logica del login y le puse validaciones a cada input

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,6 +1,42 @@
 import { Container, Form, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
+import Swal from "sweetalert2";
 
-const Login = () => {
+const Login = ({ setUsuarioLogueado }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navegacion = useNavigate();
+
+  const onSubmit = (data) => {
+    const { email, password } = data;
+
+    // simulacion de autenticacion
+    if (email === "" && password === "") {
+      setUsuarioLogueado(true);
+      Swal.fire({
+        icon: "success",
+        title: "Bienvenido!",
+        text: "Inicio de sesión exitoso.",
+        timer: 2000,
+        showConfirmButton: false,
+      });
+      // guardamos sesion en el localStorage
+      localStorage.setItem("usuarioLogueado", "true");
+      // redireccionamos al inicio
+      navegacion("/");
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Usuario o contraseña incorrectos",
+      });
+    }
+  };
+
   const manejarSubmit = (e) => {
     e.preventDefault(); // solo para que no recargue la página en el maquetado
   };
@@ -9,15 +45,31 @@ const Login = () => {
     <main className="container my-4">
       <h1 className="mb-4">Login</h1>
       <Container className="col-12 col-md-6">
-        <Form onSubmit={manejarSubmit}>
+        <Form onSubmit={handleSubmit(onSubmit) || manejarSubmit}>
           <Form.Group className="mb-3" controlId="loginEmail">
-            <Form.Label>Email o usuario</Form.Label>
-            <Form.Control type="email" placeholder="ejemplo@correo.com" />
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="ejemplo@correo.com" {...register("email",{
+              required: "El email es un dato obligatorio",
+              pattern: {
+                value:
+                  /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                message:
+                  "El email debe cumplir con el siguiente formato correo@dominio.extension",
+              }
+            })}/>
+            <Form.Text className="text-danger">{errors.email?.message}</Form.Text>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="loginPassword">
             <Form.Label>Contraseña</Form.Label>
-            <Form.Control type="password" placeholder="********" />
+            <Form.Control type="password" placeholder="********" {...register("password", {
+              required: "La contraseña es un dato obligatorio",
+              minLength: {
+                value: 6,
+                message: "La contraseña debe tener al menos 6 caracteres",
+              },
+            })}/>
+            <Form.Text className="text-danger">{errors.password?.message}</Form.Text>
           </Form.Group>
 
           <div className="mb-3">
